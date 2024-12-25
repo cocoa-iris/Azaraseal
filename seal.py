103fbda4da90ea97cc70d0860fa80de7af4929d3
虹彩 <147463615+cocoa-iris@users.noreply.github.com>
2024-12-25 13:55:19 +0900

    Update seal.py

--- seal.py
+++ seal.py
@@ -1,4 +1,24 @@
-a=0
+
+
+
+#BerryBot 1.0
+
+
+import requests
+import webbrowser
+import json
+import time
+import ast
+
+maxlen = 100
+isrm = 0
+sessions = input('session key:') 
+
+webbrowser.open("https://zinro.net/m/player.php?mode=message&to_user=ALL&message=【BerryBot】")
+
+for i in range(0,50000):
+  time.sleep(300)
+webbrowser.open("https://zinro.net/m/player.php?mode=message&to_user="+fu+"&message=<bot> あなたの権限レベルは、Lv."+str(lv)+"です。")a=0
 a2=''
 a3='ー'
 a4=''
