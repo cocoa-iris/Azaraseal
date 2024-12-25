7e8ae082b58b66d5394cbf9735297c67e8858d05
虹彩 <147463615+cocoa-iris@users.noreply.github.com>
2024-12-25 14:53:27 +0900

    Update seal.py

--- seal.py
+++ seal.py
@@ -8,42 +8,38 @@ import requests
 import webbrowser
 import json
 import time
-import ast
-
-maxlen = 100
-isrm = 0
-sessions = input('session key:') 
+import random
 
 webbrowser.open("https://zinro.net/m/player.php?mode=message&to_user=ALL&message=【BerryBot】")
 
 for i in range(0,50000):
   time.sleep(300)
-webbrowser.open("https://zinro.net/m/player.php?mode=message&to_user="+fu+"&message=<bot> あなたの権限レベルは、Lv."+str(lv)+"です。")a=0
-a2=''
-a3='ー'
-a4=''
-a5='ω'
-a6=''
-a7=''
-a8=''
-import random
-while a<=15:
-  k=random.randrange(0,5)
-  if k==0:
-    a2+=' '
-  if k==1:
-    a4+=' '
-  if k==2:
-    a6+=' '
-  if k==3:
-    a7+=' '
-  if k==4:
-    a8+=' '
-  seal='('+a2+a3+a4+a5+a6+a3+a7+'っ'+a8+')3'
-  if a==5:
-    a3='・'
-  if a==10:
-    print('(ーωーっ)3ｺﾞﾌｯ')
-    break
-  print(seal)
-  a+=1
+  a=0
+  a2=''
+  a3='ー'
+  a4=''
+  a5='ω'
+  a6=''
+  a7=''
+  a8='
+  while a<=15:
+    k=random.randrange(0,5)
+    if k==0:
+      a2+=' '
+    if k==1:
+      a4+=' '
+    if k==2:
+      a6+=' '
+    if k==3:
+      a7+=' '
+    if k==4:
+      a8+=' '
+    seal='('+a2+a3+a4+a5+a6+a3+a7+'っ'+a8+')3'
+    if a==5:
+      a3='・'
+    if a==10:
+      print('(ーωーっ)3ｺﾞﾌｯ')
+      break
+      print(seal)
+      webbrowser.open("https://zinro.net/m/player.php?mode=message&to_user=ALL&message="+seal)
+      a+=1
